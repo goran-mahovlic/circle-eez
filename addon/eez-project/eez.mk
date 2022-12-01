ab8a0123082483875aad91fc76fefb6c1d6c0d6c
Goran Mahovlić <goran.mahovlic@gmail.com>
2022-12-01 10:11:44 +0100

    demo include lvgl fix

--- addon/eez-project/eez.mk
+++ addon/eez-project/eez.mk
@@ -1,5 +1,7 @@
 CIRCLEHOME = ../..
 
+LVGL_DIR = $(CIRCLEHOME)/addon/lvgl
+
 CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.c")
 CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.cpp")
 CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
@@ -11,35 +13,11 @@ VPATH += :$(CIRCLEHOME)/addon/eez-framework/src/eez/flow
 VPATH += :$(CIRCLEHOME)/addon/eez-project
 
 CFLAGS += "-I$(CIRCLEHOME)/addon/eez-framework"
-INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(LVGL_DIR)
+INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)
 CPPFLAGS += -DEEZ_FOR_LVGL
 
-#CSRCS += lv_hal_disp.c
-#CSRCS += lv_hal_indev.c
-#CSRCS += lv_hal_tick.c
+-include $(DEPS)
 
-#DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal
-#VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal
-
-#CFLAGS += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal"
-
-#CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.c")
-#CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.cpp")
-#CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
-#CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.cpp")
-#INCLUDE += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.h")
-#INCLUDE += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.h")
-
-#INCLUDE += -I $(LVGL_DIR)
-#CPPFLAGS += -DEEZ_FOR_LVGL
-#_CFLAGS  = -I lvgl -I . -Wno-int-to-pointer-cast -Wall -Wextra -Werror -Wno-unused-parameter -Wno-pointer-sign -Wno-sign-compare -fPIC
-
-#LIBRARYDIRS = $(CIRCLEHOME)/addon/eez-framework/src \
-#	$(CIRCLEHOME)/addon/eez-framework/src/eez/flow \
-#	$(CIRCLEHOME)/addon/eez-framework
-
-#-include $(DEPS)
-
-#ifeq ($(strip $(CHECK_DEPS)),1)
-#-include $(EXOBJS:.o=.d)
-#endif
+ifeq ($(strip $(CHECK_DEPS)),1)
+-include $(EXOBJS:.o=.d)
+endif
