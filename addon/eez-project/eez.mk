b1ed29ea3b224b50348862f9c105289b3c9eade2
Goran Mahovlić <goran.mahovlic@gmail.com>
2022-12-01 12:35:04 +0100

    Adding new makefile - it now compiles c files but still missing cpp

--- addon/eez-project/eez.mk
+++ addon/eez-project/eez.mk
@@ -1,23 +1,13 @@
 CIRCLEHOME = ../..
 
-LVGL_DIR = $(CIRCLEHOME)/addon/lvgl
-
+CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
 CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.c")
 CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.cpp")
-CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
+#CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
 
-VPATH += :$(CIRCLEHOME)/addon/eez-framework
-VPATH += :$(CIRCLEHOME)/addon/eez-framework/src
-VPATH += :$(CIRCLEHOME)/addon/eez-framework/src/eez
-VPATH += :$(CIRCLEHOME)/addon/eez-framework/src/eez/flow
-VPATH += :$(CIRCLEHOME)/addon/eez-project
+CFLAGS += -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)
+CPPFLAGS += -DEEZ_FOR_LVGL -I$(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)
+#CPPFLAGS += -DEEZ_FOR_LVGL -I $(CIRCLEHOME)/addon/lvgl/lvgl
+CFLAGS += -DEEZ_FOR_LVGL -DLV_LVGL_H_INCLUDE_SIMPLE -I $(CIRCLEHOME)/addon/lvgl/lvgl
 
-CFLAGS += "-I$(CIRCLEHOME)/addon/eez-framework"
-INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)
-CPPFLAGS += -DEEZ_FOR_LVGL
-
--include $(DEPS)
-
-ifeq ($(strip $(CHECK_DEPS)),1)
--include $(EXOBJS:.o=.d)
-endif
+INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)
