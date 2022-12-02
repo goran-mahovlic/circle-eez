e94aacf87cba247427b6564e5b29b01ab9ed4353
Goran Mahovlić <goran.mahovlic@gmail.com>
2022-12-02 12:16:02 +0100

    Adding new make files

--- addon/eez-project/eez-framework.mk
+++ addon/eez-project/eez-framework.mk
@@ -1,9 +1,29 @@
 CIRCLEHOME = ../..
 
-CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.cpp")
+CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework/src -name "*.cpp")
+#CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework/src -name "*.c")
 
-CPPFLAGS += -I $(CIRCLEHOME) -I $(CIRCLEHOME)
-CPPFLAGS += -DEEZ_FOR_LVGL -DLV_LVGL_H_INCLUDE_SIMPLE -I $(CIRCLEHOME)/addon/lvgl/lvgl
+#CFLAGS += -IEEZ_FOR_LVGL -DEEZ_OPTION_GUI=0 -DLV_LVGL_H_INCLUDE_SIMPLE
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/core -I $(CIRCLEHOME)
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez -I $(CIRCLEHOME)/addon/eez-framework/src
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/flow -I $(CIRCLEHOME)/addon/eez-framework/src/eez/fs
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs -I $(CIRCLEHOME)/addon/eez-framework/src/eez/platform
+CPPFLAGS += -DEEZ_FOR_LVGL -DEEZ_OPTION_GUI=0 -DLV_LVGL_H_INCLUDE_SIMPLE -I$(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)/addon/eez-framework/src/eez/core -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui/widgets/containers -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui/widgets
 
-INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl
+INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-project/ui
+INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME) -I $(CIRCLEHOME)/addon/eez-framework/src/eez/core -I $(CIRCLEHOME)/addon/eez-framework/src/eez/fs
+INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs -I $(CIRCLEHOME)/addon/eez-framework/src/eez/platform
+INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui/widgets/containers -I $(CIRCLEHOME)/addon/eez-framework/src/eez/gui/widgets
+#CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs/agg
+#CFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs/lz4
+#CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs/lz4
+CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs
+#INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs/agg
+#INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs/lz4
+#INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/eez/libs
+#CPPFLAGS += -std=c++14
+#STANDARD ?= -std=c++14 -Wno-aligned-new
 
+#include $(CIRCLEHOME)/Rules.mk
--- addon/eez-project/eez-project.mk
+++ addon/eez-project/eez-project.mk
@@ -1,14 +1,8 @@
 CIRCLEHOME = ../..
 
 CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-project -name "*.c")
-CSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.c")
-CPPSRCS += $(shell find -L $(CIRCLEHOME)/addon/eez-framework -name "*.cpp")
 
-CFLAGS += -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)
+CFLAGS += -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src  -I $(CIRCLEHOME)/addon/eez-project/ui
 CFLAGS += -DEEZ_FOR_LVGL -DLV_LVGL_H_INCLUDE_SIMPLE -I $(CIRCLEHOME)/addon/lvgl/lvgl
 
-CPPFLAGS += -DEEZ_FOR_LVGL -I$(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src
-CPPFLAGS += -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)/addon/eez-project/ui -I $(CIRCLEHOME)
-
-INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-framework -I $(CIRCLEHOME)/addon/eez-framework/src -I $(CIRCLEHOME)/addon/eez-project/ui
-INCLUDE += -I $(CIRCLEHOME)/addon/eez-framework/src/flow -I $(CIRCLEHOME)
+INCLUDE += -I $(CIRCLEHOME)/addon/lvgl/lvgl -I $(CIRCLEHOME)/addon/eez-project/ui
